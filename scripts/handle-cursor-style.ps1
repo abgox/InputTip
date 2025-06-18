ffff2c8d4f40da8286b15ee4c0562d2316d9dbf3
abgox <abgohxf@outlook.com>
2025-06-18 09:06:09 +0800

    feat(scripts): 添加处理鼠标样式的 PowerShell 脚本
    - 添加 handle-cursor-style.ps1 脚本用于将未处理的鼠标样式文件根据映射表重命名并移动到指定输出目录

---
+++ scripts/handle-cursor-style.ps1
@@ -0,0 +1,47 @@
+﻿param(
+    # 未处理的鼠标样式目录
+    $dir,
+    # 处理后的鼠标样式目录
+    $out = "out"
+)
+
+$cursorNameMap = @{
+    AppStarting = @("Work")
+    Arrow       = @("Pointer")
+    Cross       = @("")
+    Hand        = @("Link")
+    Help        = @("")
+    IBeam       = @("Text")
+    No          = @("Unavailable")
+    Pen         = @("Handwriting")
+    Person      = @("")
+    Pin         = @("")
+    SizeAll     = @("Move")
+    SizeNESW    = @("Dgn2")
+    SizeNS      = @("Vert")
+    SizeNWSE    = @("Dgn1")
+    SizeWE      = @("Horz")
+    UpArrow     = @("Alternate")
+    Wait        = @("Busy")
+}
+
+# 输出目录
+$out_dir = Join-Path $dir $out
+New-Item -ItemType Directory $out_dir -Force
+
+Get-ChildItem $dir -Recurse -File | ForEach-Object {
+    foreach ($style in $cursorNameMap.Keys) {
+        if ($_.BaseName -eq $style -or $_.BaseName -in $cursorNameMap.$style) {
+            Move-Item $_ (Join-Path $out_dir "$($style)$($_.Extension)") -Force
+            Continue
+        }
+    }
+}
+
+
+
+
+
+
+
+
